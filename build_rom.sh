9fd23fa8ca1f33ef9b66d4d641f9b1f299d944f5
himanshu0218 <70850303+himanshu0218@users.noreply.github.com>
2021-07-11 10:56:46 +0530

    Trigger Spark-Rom (July Patch)

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch spark_surya-userdebug
+lunch spark_surya-user
 export WITH_GAPPS=true
 export TZ=Asia/Dhaka #put before last build command
 mka spark
