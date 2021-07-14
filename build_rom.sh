b3039c4174d76a9c2280e0f93f586d9d51d007c0
Lucifer-morning-star-96 <87255315+Lucifer-morning-star-96@users.noreply.github.com>
2021-07-14 07:56:33 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -2,7 +2,7 @@
 repo init --depth=1 --no-repo-verify -u git://github.com/DotOS/manifest.git -b dot11 -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/Lucifer-morning-star-96/frostmanifest.git --depth 1 -b Dotos .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
-   
+    
 # build rom 
 source build/envsetup.sh
 lunch dot_lavender-userdebug
