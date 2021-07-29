4dfa20abb865730e55db2ee0a7d4030e6feff74c
Prashant Meena <71384284+Prashant-1695@users.noreply.github.com>
2021-07-29 21:20:39 +0530

    Merge latest source & upstream kernel

--- build_rom.sh
+++ build_rom.sh
@@ -7,7 +7,6 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 . build/envsetup.sh
 lunch nezuko_lavender-userdebug
 export TZ=Asia/Dhaka #put before last build command
-export USE_GAPPS=true
 mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
