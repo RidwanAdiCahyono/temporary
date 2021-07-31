42b93a39f98712dbf7f1dd93d5ceed724b9cbd02
NganuCoeg <starterpackmeh@gmail.com>
2021-07-31 08:16:03 +0000

    Initial build

--- build_rom.sh
+++ build_rom.sh
@@ -1,11 +1,11 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/NusantaraProject-ROM/android_manifest -b 11 -g default,-device,-mips,-darwin,-notdefault
-git clone https://github.com/NganuCoeg/local_manifests --depth 1 -b RMX2030-NAD .repo/local_manifests
+git clone https://github.com/NganuCoeg/local_manifests --depth 1 -b r5x-NAD .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 . build/envsetup.sh
-lunch nad_RMX2030-userdebug
+lunch nad_r5x-userdebug
 export TZ=Asia/Jakarta
 mka nad
 
